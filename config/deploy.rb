9a4cde92f1fdec0ee89d29315db5fad1edcc7f7f
jdprgm <stickmedia@gmail.com>
2014-10-26 00:49:37 -0400

    Setting up the database

---
+++ config/deploy.rb
@@ -0,0 +1,66 @@
+require 'mina/bundler'
+require 'mina/rails'
+require 'mina/git'
+require 'mina/rvm'    # for rvm support. (http://rvm.io)
+
+# Basic settings:
+#   domain       - The hostname to SSH to.
+#   deploy_to    - Path to deploy into.
+#   repository   - Git repo to clone from. (needed by mina/git)
+#   branch       - Branch name to deploy. (needed by mina/git)
+
+set :user, 'justin'
+set :domain, '162.216.16.239'
+set :port, 444
+set :deploy_to, '/home/justin/rails_apps/tvtoday.20dots.com/'
+set :repository, 'https://github.com/justindoody/tvtoday.git'
+set :branch, 'master'
+
+set_default :rails_env, 'production'
+
+# Manually create these paths in shared/ (eg: shared/config/database.yml) in your server.
+# They will be linked in the 'deploy:link_shared_paths' step.
+set :shared_paths, ['config/database.yml', 'log']
+set :rvm_path, "/usr/local/rvm/scripts/rvm"
+
+
+# This task is the environment that is loaded for most commands, such as
+# `mina deploy` or `mina rake`.
+task :environment do
+  # For those using RVM, use this to load an RVM version@gemset.
+  invoke :'rvm:use[default]'
+end
+
+# Put any custom mkdir's in here for when `mina setup` is ran.
+# For Rails apps, we'll make some of the shared paths that are shared between
+# all releases.
+task :setup => :environment do
+  queue! %[mkdir -p "#{deploy_to}/#{shared_path}/log"]
+  queue! %[chmod g+rx,u+rwx "#{deploy_to}/#{shared_path}/log"]
+
+  queue! %[mkdir -p "#{deploy_to}/#{shared_path}/config"]
+  queue! %[chmod g+rx,u+rwx "#{deploy_to}/#{shared_path}/config"]
+
+  queue! %[touch "#{deploy_to}/#{shared_path}/config/database.yml"]
+  queue  %[echo "-----> Be sure to edit '#{deploy_to}/#{shared_path}/config/database.yml'."]
+end
+
+desc "Deploys the current version to the server."
+task :deploy => :environment do
+  deploy do
+    # Put things that will set up an empty directory into a fully set-up
+    # instance of your project.
+    invoke :'git:clone'
+    invoke :'deploy:link_shared_paths'
+    invoke :'bundle:install'
+    invoke :'rails:db_migrate'
+    invoke :'rails:assets_precompile'
+    invoke :'deploy:cleanup'
+
+    to :launch do
+      # queue "mkdir -p #{deploy_to}/#{current_path}/tmp/"
+      queue 'sudo service apache2 restart'
+    end
+  end
+end
+
