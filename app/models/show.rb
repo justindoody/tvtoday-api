c3a283a5a8a6da29c87a0c820846c5d5eb58e471
justindoody <stickmedia@gmail.com>
2015-02-16 15:10:58 -0500

    DRYing out the show load episode details method a bit

--- app/models/show.rb
+++ app/models/show.rb
@@ -41,27 +41,23 @@ class Show < ActiveRecord::Base
       # Parses episode air date and removes dashes to use as an int
       air_date = episode.at_xpath(".//FirstAired").content
 
-      name = episode.at_xpath(".//EpisodeName").content
-      description = episode.at_xpath(".//Overview").content
       season = episode.at_xpath(".//SeasonNumber").content.to_i
       episode_num = episode.at_xpath(".//EpisodeNumber").content.to_i
       season_and_episode = season_episode_string(season, episode_num)
 
+      show_details = { name: episode.at_xpath(".//EpisodeName").content,
+                       description: episode.at_xpath(".//Overview").content,
+                       season_and_episode: season_and_episode,
+                       airdate: air_date }
+
       # Set previous unless the airdate is blank or greater than todays date
       unless air_date.blank? || air_date.gsub(/-/, "").to_i >= date
-        ep[:previous][:name] = name
-        ep[:previous][:description] = description
-        ep[:previous][:season_and_episode] = season_and_episode
-        ep[:previous][:airdate] = air_date
-
+        ep[:previous] = show_details
         break # Iterate backward until we match the previous episode and quit searching
       end
 
       # Set the details for the next episode, reset each loop
-      ep[:next][:name] = name
-      ep[:next][:description] = description
-      ep[:next][:season_and_episode] = season_and_episode
-      ep[:next][:airdate] = air_date
+      ep[:next] = show_details
     end 
     return ep
   end
